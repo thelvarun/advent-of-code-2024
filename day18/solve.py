d0fd58d6e3f862633ec479e055c6b4aa0c3a1033
thelvarun <thelvarun99@gmail.com>
2025-01-14 22:13:32 -0600

    Add binary search method for day18

--- day18/solve.py
+++ day18/solve.py
@@ -1,5 +1,72 @@
 from collections import deque
 
+def solve3(coords: list[tuple[int, int]]):
+  # Create our 71 by 71 grid of characters
+  grid_size = 71
+
+
+  def is_corrupted(i: int, j: int) -> bool:
+    nonlocal grid
+    return grid[i][j]
+
+  def inb(i: int, j: int) -> bool:
+    nonlocal grid_size
+    return 0 <= i < grid_size and 0 <= j < grid_size
+  
+  # We can use a binary search to narrow down which coordinate blocks by "choosing"
+  # all of the coords, then one half and the other half, etc etc. The thing to 
+  # remember is that the coords corrupt in order. So, if the first half doesn't
+  # stop us from getting to the exit, we have to test the second half WITH
+  # the first half already marked. Apply the same logic to each subsequent step
+  # in the binary search.
+  
+  lo = 0
+  hi = len(coords)-1
+
+  # TODO: I need to name this better
+  seen_corrupted_grid = [[False for _ in range(grid_size)] for _ in range(grid_size)]
+  ans = (-1, -1)
+  while lo < hi:
+    grid = [[x for x in row] for row in seen_corrupted_grid]
+    
+    # TODO: Check for off-by-1 errors
+    mid = (lo + hi) // 2
+    for i in range(lo, mid+1):
+      coord = coords[i]
+      grid[coord[0]][coord[1]] = True
+
+    # now run our BFS... I should probably put this in a function or something...
+    # Visited 2D array for our BFS
+    visited = [[False for _ in range(grid_size)] for _ in range(grid_size)]
+
+    # Queue of coords as first two ints, number of steps as last int
+    queue: deque[tuple[int, int, int]] = deque()
+    queue.append((0, 0, 0))
+    visited[0][0] = True
+    can_reach_exit = False
+    while queue:
+      cur = queue.popleft()
+      coord = cur[0], cur[1]
+      if (coord == (grid_size-1, grid_size-1)):
+        can_reach_exit = True
+        break
+      # go through our neighbors, make sure they're inbounds, then
+      for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        next_pos = (coord[0] + delta[0], coord[1] + delta[1])
+        if inb(*next_pos) and not is_corrupted(*next_pos) and not visited[next_pos[0]][next_pos[1]]:
+          queue.append((next_pos[0], next_pos[1], cur[2] + 1))
+          visited[next_pos[0]][next_pos[1]] = True
+
+    # This means the first half of the corrupted bytes can still allow us to reach
+    # the exit. But since the bytes fall in order, we need to keep those coordinates
+    # marked as corrupted.
+    if can_reach_exit:
+      lo = mid+1
+      seen_corrupted_grid = grid
+    else:
+      hi = mid
+  print(coords[lo]) # these are reversed.
+
 
 def solve2(coords: list[tuple[int, int]]):
   # Create our 71 by 71 grid of characters
@@ -36,17 +103,7 @@ def solve2(coords: list[tuple[int, int]]):
   for i in range(len(coords)):
     # Now mark the next corrupted byte
     cur_corrupted_coord = coords[i]
-    # print("marking", cur_corrupted_coord, "as corrupted")
     grid[cur_corrupted_coord[0]][cur_corrupted_coord[1]] = True
-    # print("marking char grid corrupted coord", cur_corrupted_coord, "as corrupted")
-    # char_grid[cur_corrupted_coord[0]][cur_corrupted_coord[1]] = '#'
-
-    # # Create a copy of the grid for the bfs
-    # char_grid_copy = [[x for x in row]for row in char_grid]
-    # def print_char_grid():
-    #   nonlocal char_grid_copy
-    #   for row in char_grid_copy:
-    #     print(row)
 
     # Visited 2D array for our BFS
     visited = [[False for _ in range(grid_size)] for _ in range(grid_size)]
@@ -56,16 +113,9 @@ def solve2(coords: list[tuple[int, int]]):
     queue.append((0, 0, 0))
     visited[0][0] = True
     can_reach_exit = False
-    # char_grid_copy[0][0] = 'O'
-    # print("---beginning---")
     while queue:
       cur = queue.popleft()
-      # print("cur in queue", cur)
       coord = cur[0], cur[1]
-      # Mark where we are
-      # char_grid_copy[coord[0]][coord[1]] = 'O'
-      # print_char_grid()
-      # input()
       if (coord == (grid_size-1, grid_size-1)):
         can_reach_exit = True
         break
@@ -135,4 +185,4 @@ with open("day18.in") as f:
     coord = tuple(int(x) for x in line.split(','))
     coord = coord[1], coord[0]
     coords.append(coord)
-solve2(coords)
+solve3(coords)
